548e8dc21b48861d5755a517f5f7fdba19cc31f3
DavidMayorgaS08 <david08102@hotmail.com>
2023-11-20 22:40:50 -0500

    Formulario

---
+++ main.js
@@ -0,0 +1,11 @@
+function registrar(){
+    let formulario = document.querySelector('.cont_for');
+
+    formulario.style.top = '15px';
+}
+
+function cerrar(){
+    let formulario = document.querySelector('.cont_for');
+
+    formulario.style.top = '-100vh';
+}
