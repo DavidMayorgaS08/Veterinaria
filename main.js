b34aa2bd9624d7b9643333e797e16fa806716fba
DavidMayorgaS08 <david08102@hotmail.com>
2023-12-07 11:23:05 -0500

    Funcionalidad completa

--- main.js
+++ main.js
@@ -103,17 +103,45 @@ function enviar() {
     let card = document.createElement('div');
     card.className = 'card';
     card.innerHTML = `
-        <img class="img_mascota" src="${img}">
-        <p class="text_tipo">${mascota}</p>
-        <p class="text_mascota">${nombreMascota}</p>
-        <p class="text_propietario">${nombrePropietario}</p>
-        <p class="text_fecha">${fechaCita}</p>
-        <p class="text_hora">${horaCita}</p>
-        <p class="text_sintomas">${sintomas}</p>
+    <div class="cont_img_card">
+        <img class="img_mascota_card" src="${img}">
+    </div>
+    <div class="cont_tipo_card">
+        <p class="text_titulo_tipo_card">Tipo:</p>
+        <p class="text_tipo_card">${mascota}</p>
+    </div>
+    <div class="cont_mascota_card">
+        <p class="text_titulo_mascota_card">Nombre:</p>
+        <p class="text_mascota_card">${nombreMascota}</p>
+    </div>
+    <div class="cont_propietario_card">
+        <p class="text_titulo_propietario_card">Propietario:</p>
+        <p class="text_propietario_card">${nombrePropietario}</p>
+    </div>
+    <div class="cont_telefono_card">
+        <p class="text_titulo_telefono_card">Telefono:</p>
+        <p class="text_telefono_card">${telefono}</p>
+    </div>
+    <div class="cont_fecha_card">
+        <p class="text_titulo_fecha_card">Fecha:</p>
+        <p class="text_fecha_card">${fechaCita}</p>
+    </div>
+    <div class="cont_hora_card">
+        <p class="text_titulo_hora_card">Hora:</p>
+        <p class="text_hora_card">${horaCita}</p>
+    </div>
+    <div class="cont_sintomas_card">
+        <p class="text_titulo_sintomas_card">Sintomas:</p>
+        <p class="text_sintomas_card">${sintomas}</p>
+    </div>
+    <div class="cont_botones_card">
         <button class="btn_activar" onclick="abrirCard(${registro.id})">Activar</button>
         <button class="btn_cerrar_card" onclick="cerrarCard(${registro.id})">Cerrar</button>
+    </div>
+    <div class="cont_botones_card">
         <button class="btn_anular" onclick="anularCard(${registro.id})">Anular</button>
         <button class="btn_editar" onclick="editarCard(${registro.id})">Editar</button>
+    </div>
     `;
 
     let contActivas = document.querySelector('.cont_activas');
@@ -166,28 +194,6 @@ function renderizarCards() {
     registrosActivos.forEach(registro => {
         let card = registro.card.cloneNode(true);
 
-        card.querySelectorAll('button').forEach(btn => btn.remove());
-
-        let btnAbrir = document.createElement('button');
-        btnAbrir.textContent = 'Activar';
-        btnAbrir.onclick = () => abrirCard(registro.id);
-        card.appendChild(btnAbrir);
-
-        let btnCerrar = document.createElement('button');
-        btnCerrar.textContent = 'Cerrar';
-        btnCerrar.onclick = () => cerrarCard(registro.id);
-        card.appendChild(btnCerrar);
-
-        let btnAnular = document.createElement('button');
-        btnAnular.textContent = 'Anular';
-        btnAnular.onclick = () => anularCard(registro.id);
-        card.appendChild(btnAnular);
-
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Editar';
-        btnEditar.onclick = () => editarCard(registro.id);
-        card.appendChild(btnEditar);
-
         if (registro.estado === 'activa') {
             contActivas.appendChild(card);
         } else if (registro.estado === 'cerrada') {
@@ -204,7 +210,7 @@ function verActivas() {
     let cerradas = document.querySelector('.cont_cerradas');
     let anuladas = document.querySelector('.cont_anuladas');
 
-    activas.style.display = "flex";
+    activas.style.display = "grid";
     cerradas.style.display = "none";
     anuladas.style.display = "none";
 }
@@ -215,7 +221,7 @@ function verCerradas() {
     let anuladas = document.querySelector('.cont_anuladas');
 
     activas.style.display = "none";
-    cerradas.style.display = "flex";
+    cerradas.style.display = "grid";
     anuladas.style.display = "none";
 }
 
@@ -226,7 +232,7 @@ function verAnuladas() {
 
     activas.style.display = "none";
     cerradas.style.display = "none";
-    anuladas.style.display = "flex";
+    anuladas.style.display = "grid";
 }
 
 function editarCard(id) {
@@ -245,7 +251,7 @@ function mostrarFormularioEdicion(registro, index) {
     formularioEdicion.querySelector('#nombreMascota').value = registro.nombreMascota;
     formularioEdicion.querySelector('#nombrePropietario').value = registro.nombrePropietario;
     formularioEdicion.querySelector('#telefono').value = registro.telefono;
-    let mascotaId;
+    let mascotaId = 0;
     if (registro.tipoMascota === "Perro") {
         mascotaId = 1;
     } else if (registro.tipoMascota === "Gato") {
@@ -277,8 +283,28 @@ function actualizarRegistro(registro, index) {
     registro.horaCita = document.getElementById('horaCita').value;
     registro.sintomas = document.getElementById('sintomas').value;
 
-    registro.card.querySelector('.text_mascota').textContent = registro.nombreMascota;
-    registro.card.querySelector('.text_propietario').textContent = registro.nombrePropietario;
+    registro.card.querySelector('.text_mascota_card').textContent = registro.nombreMascota;
+    registro.card.querySelector('.text_propietario_card').textContent = registro.nombrePropietario;
+    registro.card.querySelector('.text_fecha_card').textContent = registro.fechaCita;
+    registro.card.querySelector('.text_hora_card').textContent = registro.horaCita;
+    registro.card.querySelector('.text_sintomas_card').textContent = registro.sintomas;
+    registro.card.querySelector('.text_telefono_card').textContent = registro.telefono;
+    if (registro.tipoMascota === '1') {
+        registro.card.querySelector('.text_tipo_card').textContent = 'Perro';
+        registro.card.querySelector('.img_mascota_card').src = './img/img perro.jpg';
+    }
+    else if (registro.tipoMascota === '2') {
+        registro.card.querySelector('.text_tipo_card').textContent = 'Gato';
+        registro.card.querySelector('.img_mascota_card').src = './img/img gato.jpg';
+    }
+    else if (registro.tipoMascota === '3') {
+        registro.card.querySelector('.text_tipo_card').textContent = 'Ave';
+        registro.card.querySelector('.img_mascota_card').src = './img/img ave.jpg';
+    }
+    else {
+        registro.card.querySelector('.text_tipo_card').textContent = 'Otro';
+    }
+    
     
     let formularioEdicion = document.querySelector('.cont_for');
     let btnEnviar = formularioEdicion.querySelector('.btn_enviar');
@@ -286,4 +312,5 @@ function actualizarRegistro(registro, index) {
         enviar();
     };
     cerrar();
+    renderizarCards();
 }
